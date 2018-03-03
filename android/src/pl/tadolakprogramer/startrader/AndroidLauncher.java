1a75c0371f8709fbe4c85a4b1ec9628b32b35ca3
Tadolak <tadolak@gmail.com>
2018-03-03 20:19:20 +0100

    Initial commit

---
+++ android/src/pl/tadolakprogramer/startrader/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package pl.tadolakprogramer.startrader;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import pl.tadolakprogramer.startrader.StarTrader;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StarTrader(), config);
+	}
+}
---
+++ desktop/src/pl/tadolakprogramer/startrader/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.tadolakprogramer.startrader.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.tadolakprogramer.startrader.StarTrader;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarTrader(), config);
+	}
+}
